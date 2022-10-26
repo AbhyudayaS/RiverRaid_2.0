91de3c9eb13c377a97593d23bb7ba5314bc294cd
AbhyudayaSardesai <gd06abhyudaya@thecea.ca>
2022-10-26 11:50:53 -0700

    Basic Plane Controller

--- Assets/RiverRaid/Scripts/Scripts/BaseAirplaneController.cs
+++ Assets/RiverRaid/Scripts/Scripts/BaseAirplaneController.cs
@@ -15,11 +15,14 @@ namespace RiverRaid
         [SerializeField]
         protected float controlSpeed = 0f;
         [SerializeField]
-        protected float throttle = 0f;       
+        protected float throttle = 0f;     
+        [SerializeField]
+        private float controlRollFactor;
 
         private InputListener _inputListener;
         private Rigidbody _rb;
         private Vector3 _rotation;
+
         #endregion
 
         private void Start()
@@ -27,39 +30,47 @@ namespace RiverRaid
             _inputListener = GetComponent<InputListener>();
             _rb = GetComponent<Rigidbody>();
         }
+
         private void Update()
         {
             HandleInput();
+            ResetZRot();
+            ProcessRotaion();
         }
 
         private void FixedUpdate()
         {
-            MoveForward();
+            MoveForward();          
+        }
+
+        private void ResetZRot()
+        {
+            if (xThrow == 0 && yThrow == 0)
+            {
+                var targetRot = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 0);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, 3f * Time.deltaTime);
+            }
+        }
+
+        private void ProcessRotaion()
+        {
+            if (xThrow == 0 && yThrow == 0) return;           
+            float roll = xThrow * controlRollFactor;
+            transform.localRotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, roll);
         }
 
         private void MoveForward()
-        {            
-            transform.Rotate(_rotation);
+        {
             _rb.velocity = transform.forward * throttle;
+            transform.Rotate(_rotation);
         }
 
         private void HandleInput()
         {
             xThrow = _inputListener.move.x;
             yThrow = _inputListener.move.y;
-
             //transform.position += new Vector3(0f, yThrow, 0) * throttle * Time.fixedDeltaTime;
-
-            if (xThrow != 0)
-            {
-                _rotation = new Vector3(yThrow, xThrow, 0f ) *  throttle * controlSpeed * Time.deltaTime;
-              
-            }
-            else
-            {
-                _rotation = Vector3.zero;
-            }
-
+            _rotation = new Vector3(yThrow, xThrow, 0f) * controlSpeed * Time.deltaTime;
         }
     }
 }
